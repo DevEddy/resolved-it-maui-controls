d2b329b72ff80f34405840167a8bd32cda13d7fd
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2023-03-24 14:57:27 +0100

    added login validation logic and sing in button to the sample app

---
+++ src/Resolved.It.Maui.App/BasePageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Resolved.It.Maui.App;
+
+public class BasePageViewModel : ObservableObject
+{
+    private readonly SemaphoreSlim _isBusyLock = new(1, 1);
+    
+    private bool _isBusy;
+    public bool IsBusy
+    {
+        get => _isBusy;
+        private set => SetProperty(ref _isBusy, value);
+    }
+    
+    protected async Task IsBusyFor(Func<Task> unitOfWork)
+    {
+        await _isBusyLock.WaitAsync();
+
+        try
+        {
+            IsBusy = true;
+
+            await unitOfWork();
+        }
+        finally
+        {
+            IsBusy = false;
+            _isBusyLock.Release();
+        }
+    }
+}
--- src/Resolved.It.Maui.App/MainPageViewModel.cs
+++ src/Resolved.It.Maui.App/MainPageViewModel.cs
@@ -1,14 +1,45 @@
-﻿using Resolved.It.Maui.Core.Validations;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Resolved.It.Maui.Core.Validations;
 using Resolved.It.Maui.Core.Validations.Rules;
 
 namespace Resolved.It.Maui.App;
 
-public class MainPageViewModel
-{
+public partial class MainPageViewModel : BasePageViewModel
+{    
+    [ObservableProperty]
+    private bool _isValid;
     public ValidatableValue<string> Email { get; } = new();
     public ValidatableValue<string> Password { get; } = new();
 
     public MainPageViewModel()
+    {
+        AddValidations();
+    }
+    private bool IsInputValid() => IsValid;
+    
+    [RelayCommand(CanExecute = nameof(IsInputValid))]
+    private async Task SignInAsync()
+    {
+        await IsBusyFor(
+            async () =>
+            {
+                await Task.Delay(10);
+                IsValid = true;
+            });
+    }
+    
+    private void CheckValidation()
+    {
+        var isEmailValid = Email.IsValid;
+        var isPasswordValid = Password.IsValid;
+
+        IsValid = isEmailValid && isPasswordValid;
+
+        SignInCommand.NotifyCanExecuteChanged();
+    }
+
+    private void AddValidations()
     {
         Email.Validations.Add(new EmailRule<string>
         {
@@ -19,5 +50,17 @@ public class MainPageViewModel
         {
             ValidationMessage = "Password is required"
         });
+        
+        Email.OnValidated = _ =>
+        {
+            Password.Validate(false);
+            CheckValidation();
+        };
+        
+        Password.OnValidated = _ =>
+        {
+            Email.Validate(false);
+            CheckValidation();
+        };
     }
 }
