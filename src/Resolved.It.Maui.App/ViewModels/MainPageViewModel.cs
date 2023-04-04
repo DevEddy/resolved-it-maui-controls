ffc24d832fdf3c530452fc97ead2c110dfa8d8fe
Eduard Schäfer <m.eduard.schaefer@gmail.com>
2023-04-04 11:29:55 +0200

    added a date picker to the sample app

--- src/Resolved.It.Maui.App/ViewModels/MainPageViewModel.cs
+++ src/Resolved.It.Maui.App/ViewModels/MainPageViewModel.cs
@@ -11,16 +11,17 @@ namespace Resolved.It.Maui.App.ViewModels;
 public partial class MainPageViewModel : BasePageViewModel
 {
     public ObservableCollection<Country> Countries { get; } = new(Country.GetCountryList());
-
     public ValidatableValue<string> Note { get; } = new();
     public ValidatableValue<Country> SelectedCountry { get; } = new();
     public ValidatableValue<string> Name { get; } = new();
     public ValidatableValue<string> Description { get; } = new();
+    public ValidatableValue<DateTime> Timestamp { get; } = new();
 
     public MainPageViewModel(INavigationService navigationService)
         : base(navigationService)
     {
         SelectedCountry.Validations.Add(new IsValidCountryRule<Country> { ValidationMessage = "Land is required." });
+        Timestamp.Value = DateTime.Now;
     }
     
     [RelayCommand]
